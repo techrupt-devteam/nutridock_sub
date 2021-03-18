9255224d6820501ee9675f311349d76e58189478
techrupt-devteam <developer@techrupt.in>
2021-03-18 11:42:34 +0530

    added active class in side bar

--- resources/views/layouts/subscriber_sidebar.blade.php
+++ resources/views/layouts/subscriber_sidebar.blade.php
@@ -1,24 +1,19 @@
 <div class="col-md-4 col-lg-3">
 	<div class="user-left-nav">
 		<ul class="list-unstyled user-nav-list">
-			<li class="list-item active">
+			<li class="list-item {{ Request::is('profile') ? 'active' : '' }}">
 				<a href="profile" class="link"> <i class="icon fa icon fa-user-circle-o"></i>My Profile</a>
 			</li>
-			<li class="list-item">
+			<li class="list-item {{ Request::is('mysubscription') ? 'active' : '' }}" >
                 <a href="mysubscription" class="link"><i class="fa fa-shopping-basket"></i> My Subscriptions</a> 
             </li>
-			<li class="list-item ">
+			<li class="list-item {{ Request::is('mealprogram') ? 'active' : '' }}">
 				<a href="mealprogram" class="link"><i class="icon fa icon fa-cutlery"></i>Meal Prograram </a>
 			</li>
-			<li class="list-item">
+			<li class="list-item {{ Request::is('chat') ? 'active' : '' }}">
                 <a href="{{ URL('') }}/chat" class="link">
 					<i class="fa fa-weixin"></i> Chat with Nutrionist</a> 
-            </li>
-			<li class="list-item">
-                <a href="{{ URL('') }}/mysubscription" class="link">
-					<i class="fa fa-commenting"></i> Chat with Help Desk</a> 
-            </li>
-						
+            </li>	
 			<li class="list-item"><span class="link">
                 <i class="icon fa icon fa-sign-out"></i>
                 <a class="dropdown-action-item" href="{{ URL('') }}/logout">
